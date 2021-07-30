9d945c91d3a4d3d1406b6ad54ac77718a8acf892
Brian <garyramirez007@gmail.com>
2021-07-30 16:10:22 -0600

    create JPA project, stub and test PregnancyTest, stub REST boot project, link project dependencies

---
+++ JPAPregnancy/src/main/java/com/skilldistillery/pregnancyapp/entities/Pregnancy.java
@@ -0,0 +1,66 @@
+package com.skilldistillery.pregnancyapp.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pregnancy {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private int weeks;
+
+	public Pregnancy() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getWeeks() {
+		return weeks;
+	}
+
+	public void setWeeks(int weeks) {
+		this.weeks = weeks;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pregnancy [id=").append(id).append(", weeks=").append(weeks).append("]");
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pregnancy other = (Pregnancy) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
---
+++ PregnancyTracker/src/main/java/com/skilldistillery/pregnancyapp/PregnancyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.pregnancyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PregnancyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PregnancyTrackerApplication.class, args);
+	}
+
+}
